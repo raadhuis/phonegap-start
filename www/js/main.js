76328115b027018e1307cb108ce54a9652ae9a6c
Jorgen <jorgen@raadhuis.com>
2015-07-08 15:00:21 +0200

    Some css + randomize JS array

--- www/js/main.js
+++ www/js/main.js
@@ -4,3 +4,13 @@ $(document).bind("mobileinit", function(){
        $.mobile.defaultPageTransition = 'flow';
   
 });
+
+$(document).ready(function() {
+     
+    $(".button").click(function() {
+         var messages = ["Frikandel!", "Kaassoufle", "Bal gehakt", "Halve kip", "Huzarenslaatje"],
+         message = messages[Math.floor(Math.random() * messages.length)];
+        $("#snack").text(message); 
+    });
+    
+});
